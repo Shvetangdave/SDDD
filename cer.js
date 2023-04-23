b208969ef68ffb5f04c1e3b63ad97931fd0651b8
Devarsh <87245954+Shvetangdave@users.noreply.github.com>
2023-04-23 14:55:38 -0400

    Add files via upload

---
+++ cer.js
@@ -0,0 +1,11 @@
+function back(){
+    window.location='plu.html';
+    }
+       
+    function next(){
+    window.location='make.html';
+    }
+
+    function home(){
+        window.location='inesolar.html';
+        }
---
+++ mer.js
@@ -0,0 +1,11 @@
+function back(){
+ window.location='sun.html';
+}
+
+function next(){
+window.location='venus.html';
+}
+
+function home(){
+    window.location='inesolar.html';
+    }
